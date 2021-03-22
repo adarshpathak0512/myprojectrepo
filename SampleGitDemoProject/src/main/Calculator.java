2588f8bb3d6e2154a377df70394ffc8dfcf13e9d
Adarsh <Adarsh@LAPTOP-8MRD2BAV>
2021-03-22 18:09:31 +0530

    Second Commit!

--- SampleGitDemoProject/src/main/Calculator.java
+++ SampleGitDemoProject/src/main/Calculator.java
@@ -3,8 +3,11 @@ package main;
 public class Calculator {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hello World");
 
 	}
-
+	
+	public static void add(int num1,int num2){
+	      System.out.println(num1+num2);
+	  }
 }
